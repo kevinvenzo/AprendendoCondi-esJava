a9674af39e3280f2464286302529b941879def55
Guuu00 <ratodorginha@gmail.com>
2024-08-21 20:46:31 -0300

    Criação do projeto Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Integer idadePessoa = 20;
+        String nomePessoa = "kevin";
+
+        if (idadePessoa > 20) ;
+        System.out.println("é maior que 20");
+    }else{
     }
+  }
+
 }
